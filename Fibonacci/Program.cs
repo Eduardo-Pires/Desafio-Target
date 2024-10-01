b45039c56a4e6974c7d1c9b3db1df26abcc80126
Eduardo Pires <94654833+Eduardo-Pires@users.noreply.github.com>
2024-10-01 17:33:16 -0300

    exercício 1 concluído

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    public static bool IsInFibonacci(int n)
+    {
+        int x = 1, y = 0, temp;
+
+        while (x < n)
+        {
+            temp = x;
+            x += y;
+            y = temp;
+        }
+
+        return x == n;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Exercício 1: Esse número está na sequência de Fibonacci?");	
+        Console.Write("Digite um número: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        if (IsInFibonacci(n))
+        {
+            Console.WriteLine("Sim, o número está na sequência de Fibonacci.");
+        }
+        else
+        {
+            Console.WriteLine("Não, o número não está na sequência de Fibonacci.");
+        }
+    }
+}
+
