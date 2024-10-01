7b5750e470ad0108dba2dacb56c845523960e1d9
Eduardo Pires <94654833+Eduardo-Pires@users.noreply.github.com>
2024-10-01 17:33:30 -0300

    exercício 2 concluído

---
+++ TemA/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using System;
+
+class Program
+{
+    public static void IsThereAnA(string text)
+    {
+        bool contem = text.Contains('a') || text.Contains('A');
+        if (contem)
+        {
+            Console.WriteLine($"O texto contém a letra a maiúscula ou minúscula {text.Count(c => c == 'a' || c == 'A')} vezes");
+        }
+        else
+        {
+            Console.WriteLine("O texto não contém a letra 'A'");
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        string filePath = @"consulta.txt";
+        
+        try
+        {
+            // Lê todo o conteúdo do arquivo
+            string content = File.ReadAllText(filePath);
+            IsThereAnA(content);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Erro ao ler o arquivo: {e.Message}");
+        }
+    }
+}
+
+
